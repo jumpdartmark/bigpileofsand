fe8001c646f93091a64eeecb17f35abb890b38c9
jumpdart <markhoward000@gmail.com>
2021-11-29 00:01:30 -0500

    layout style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
 import AppLayout from './components/AppLayout';
 
+import styles from './App.module.scss';
+
 const App: React.FC = () => (
-  <div className="App">
+  <div className={styles.app}>
     <AppLayout>
       <h1>Hello dune world</h1>
     </AppLayout>
--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
 import {
   UserOutlined,
@@ -6,43 +6,47 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 
+import styles from './AppLatout.module.scss';
+
 const {
   Header, Content, Footer, Sider,
 } = Layout;
 
-const AppLayout: React.FC = ({ children }) => (
-  <Layout>
-    <Sider
-      style={{
-        overflow: 'auto',
-        height: '100vh',
-        position: 'fixed',
-        left: 0,
-      }}
-    >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          nav 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-          nav 2
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UploadOutlined />}>
-          nav 3
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout className="site-layout" style={{ marginLeft: 200 }}>
-      <Header className="site-layout-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
-          {children}
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+const AppLayout: React.FC = ({ children }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const onCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+  return (
+    <Layout>
+      <Sider className={styles.sider} collapsible collapsed={isCollapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content className={styles.content}>
+          <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+            <div className={styles.whatever}>
+              whatever
+            </div>
+            {children}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
     </Layout>
-  </Layout>
-);
+  );
+};
 
 export default AppLayout;
